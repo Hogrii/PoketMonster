1007e99dcdfee87b9d8ca69ae110b18423b669d5
LJH <hosboy93@naver.com>
2023-04-07 15:13:05 +0900

    feat(User) : 용돈 추가
    - 소지금 변수
    - 주석 추가

--- src/User/User.java
+++ src/User/User.java
@@ -6,11 +6,13 @@ public class User {
 	private String userName;
 	private int monsterBall;
 	private ArrayList<String> catchPoketmon = new ArrayList<>();
+	private int money;
 	
 	// 유저 이름 저장
 	public void inputUserName(String userName) {
 		this.userName = userName;
 		this.monsterBall = 5;
+		this.money = 0;
 	}
 	
 	// 유저 이름 반환
@@ -18,10 +20,12 @@ public class User {
 		return this.userName;
 	}
 	
+	// 야생포켓몬 포획 시도
 	public void throwBall() {
 		this.monsterBall--;
 	}
 	
+	// 야생포켓몬 포획
 	public void catchPoketmon(String catchPoketmon) {
 		this.catchPoketmon.add(catchPoketmon);
 	}
